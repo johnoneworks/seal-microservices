07909cea72c01005e8fa9a9414e42204fdb00e8d
john.huang <john.huang@onelab.tw>
2022-07-13 13:32:35 +0800

    Data/Repository layer

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        // this is .net core dependency injection
+        // will retrieve the configuration in ./appsettings.json
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        }
+
+        public IMongoCollection<Product> Products => throw new NotImplementedException();
+    }
+}
