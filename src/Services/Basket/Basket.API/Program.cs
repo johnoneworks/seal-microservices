e791c5c029d0207479125d68425d0a159b3bf2e2
john.huang <john.huang@onelab.tw>
2022-07-21 14:02:28 +0800

    basket implementation

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,5 @@
+using Basket.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +8,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// services by us
 // add redis related
 builder.Services.AddStackExchangeRedisCache(options =>
 {
     // this gets the JSON defined values from appsettings.json
     options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString"); 
 });
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
 var app = builder.Build();
 
